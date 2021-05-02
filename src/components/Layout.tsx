91d23abb6659d3009d12d0860a68fc86832a6738
Markus Dethlefsen <markus.dethlefsen@finanzcheck.de>
2021-05-02 16:06:19 +0200

    feat: add some more layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,52 +1,117 @@
-import { ReactNode, FC } from 'react'
+import { ReactNode, FC, useState } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
-import P from './elements/P'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  footer: {
+    position: 'fixed',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor: theme.palette.grey[200]
+  }
+}))
+
 const Layout: FC<Props> = ({
   children,
   title = 'This is the default title'
-}) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <P>
-          <Link href="/">
-            <a>Home</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/about">
-            <a>About</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/diabetes">
-            <a>Diabetes List</a>
-          </Link>{' '}
-          | <a href="/api/users">Users API</a>
-        </P>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I&apos;m here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+}) => {
+  const classes = useStyles()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div className={classes.root}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">Diabetes Tracker</Typography>
+          </Toolbar>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right'
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right'
+            }}
+            open={open}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="/users">
+                <a>Users List</a>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="/diabetes">
+                <a>Diabetes List</a>
+              </Link>
+            </MenuItem>
+          </Menu>
+        </AppBar>
+      </header>
+      {children}
+      <footer className={classes.footer}>
+        <span>I&apos;m here to stay (Footer)</span>
+      </footer>
+    </div>
+  )
+}
 
 export default Layout
--- src/pages/diabetes/index.tsx
+++ src/pages/diabetes/index.tsx
@@ -3,7 +3,6 @@ import { GetServerSideProps } from 'next'
 import Link from 'next/link'
 
 import Layout from '../../components/Layout'
-import H2 from '../../components/elements/H2'
 import P from '../../components/elements/P'
 import DiabetesList from '../../components/DiabetesList'
 import { fetchSheet } from '../../lib/fetchSheet'
@@ -13,11 +12,6 @@ type Props = DiabetesData
 
 const WithServerSideProps: FC<Props> = (props: Props) => (
   <Layout title="Diabetes Data">
-    <H2>Diabetes Data</H2>
-    <P>
-      Example fetching data from inside <code>getServerSideProps()</code>.
-    </P>
-    <P>You are currently on: /diabetes</P>
     <DiabetesList {...props} />
     <P>
       <Link href="/">
